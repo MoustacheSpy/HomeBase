8bddbb41b0f6c83af30befc24c1a963c85559941
MoustacheSpy <MoustacheSpy@hotmail.com>
2016-09-19 18:45:21 +0200

    Basic working ik

--- HomeBase/HomeBase.cpp
+++ HomeBase/HomeBase.cpp
@@ -3,8 +3,8 @@
 
 #pragma once
 #include "stdafx.h"
-#include <SDL.h>
-#include <SDL_image.h>
+#include "SDL.h"
+#include "SDL_image.h"
 #include "ik.h"
 #define SCREENWIDTH 600
 #define SCREENHEIGHT 600
@@ -30,6 +30,7 @@ int initSDL(SDL_Window * window, SDL_Surface* screenSurface,SDL_Renderer *render
 		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREENWIDTH, SCREENHEIGHT, SDL_WINDOW_SHOWN);
 		if (window == NULL)
 		{
+
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
 			return 0;
 		}
@@ -45,12 +46,25 @@ int initSDL(SDL_Window * window, SDL_Surface* screenSurface,SDL_Renderer *render
 	
 }
 
-SDL_Point rotate_point(float cx, float cy, float angle, SDL_Point p)
+SDL_Point rotate_point(double cx, double cy, double angle, SDL_Point p)
 {
-	
-	p.x = cx + (p.x - cx)*cos(angle) - (p.y - cy)*sin(angle);
+	double pi = acos(-1);
+	double rotation_angle = (double)angle / 180.0 * pi;
+	double s = sin(rotation_angle);
+	double c = cos(rotation_angle);
 
-	p.y = cy + (p.x - cx)*sin(angle) + (p.y - cy)*cos(angle);
+	// translate point back to origin:
+	p.x -= cx;
+	p.y -= cy;
+
+	// rotate point
+	double xnew = p.x * c - p.y * s;
+	double ynew = p.x * s + p.y * c;
+
+	// translate point back:
+	p.x = xnew + cx;
+	p.y = ynew + cy;
+	
 	return p;
 }
 double get_degrees(double input)
@@ -95,71 +109,63 @@ int main(int argc, char* args[]){
 		}
 
 	}
-	
-	bool quit = false;
+	SDL_Surface *roboArm1 = NULL;
+	SDL_Texture *roboArm1T = NULL;
+	roboArm1 = IMG_Load("robot arm.png");
+	roboArm1T = SDL_CreateTextureFromSurface(renderer, roboArm1);
+	SDL_Rect dsrect1 = { 0,25,100,50 };
+	SDL_Rect dsrect2 = { 100,25,100,50 };
+	double angle1=0.1, angle2=0,oldangle1=0,oldangle2=0;
+	SDL_Point line11 = { 10,0 }, line12 = { 110,0 }, line21 = { 110,0 }, line22 = { 210,0 };
 	SDL_Event event;
-	if (1) {
-		printf("init successful");
-		SDL_Surface* roboBone1S = NULL;
-		SDL_Texture* roboBone1 = NULL;
-		SDL_Surface* roboBone2S = NULL;
-		SDL_Texture* roboBone2 = NULL;
-		roboBone1S = IMG_Load("robot arm.png");
-		if (roboBone1S == NULL)
-			printf("couldnt load robo arm. ERROR %s \n", IMG_GetError());
-		roboBone2S = IMG_Load("robot arm.png");
-		if (roboBone2S == NULL)
-			printf("couldnt load robo arm. ERROR %s \n", IMG_GetError());
-		roboBone1 = SDL_CreateTextureFromSurface(renderer, roboBone1S);
-		if (roboBone1 == NULL)
-			printf("Couldnt create Robo bone texture! Error : %s\n", IMG_GetError());
-		roboBone2 = SDL_CreateTextureFromSurface(renderer, roboBone2S);
-		if (roboBone2 == NULL)
-			printf("Couldnt create Robo bone texture! Error : %s\n", IMG_GetError());
-		double angle1 = 0;
-		double angle2 = 0;
-		double lenght1 = 200;
-		double lenght2 = 200;
-		SDL_Rect dsrect1 = { 50,10,100,50 };
-		SDL_Rect dsrect2 = { 0,0,100,50 };
-		SDL_Point p = { 0,0 };
-		SDL_Point childCor = { 100,0 };
-		int mousex = 0, mousey = 0;
-		double oldAngle = angle2;
-		while (1)
-		{
-			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-			SDL_RenderClear(renderer);
-			SDL_GetMouseState(&mousex, &mousey);
-			solveIK(&angle1, &angle2, false, dsrect1.w, dsrect2.w, mousex, mousey);
-			angle1 = get_degrees(angle1);
-			angle2 = get_degrees(angle2);
-			if (angle1 != oldAngle) {
-				childCor = rotate_point(dsrect1.x, dsrect1.y, (oldAngle - angle1), childCor);
-				oldAngle = angle1;
-			}
-			SDL_RenderCopyEx(renderer, roboBone1, NULL, &dsrect1, angle1, &p, SDL_FLIP_NONE);
-			SDL_SetRenderDrawColor(renderer, 0, 255, 0, 0);
-			SDL_RenderDrawLine(renderer, childCor.x - 10, childCor.y - 10, childCor.x + 10, childCor.y + 10);
-			SDL_RenderDrawLine(renderer, childCor.x + 10, childCor.y - 10, childCor.x - 10, childCor.y + 10);
-			
-			dsrect2.x = childCor.x;
-			dsrect2.y = childCor.y;
-
-			//SDL_RenderCopyEx(renderer, roboBone2, NULL, &dsrect2, angle2, &p, SDL_FLIP_NONE);
-			
-			SDL_RenderPresent(renderer);
-			SDL_PollEvent(&event);
-			SDL_Delay(16);
-
-
-			// SDL_QUIT event has come in, quit. 
-			if (event.type == SDL_QUIT)
-			{
-				quit = true;
+	int mousex = 0, mousey = 0;
+	while (SDL_PollEvent(NULL)) {
+		 
+		SDL_GetMouseState(&mousex, &mousey);
+		if (GetAsyncKeyState('W'))
+			angle1 += 0.1;
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+		SDL_RenderClear(renderer);
+		if (angle1 < oldangle1-0.01||angle1 > oldangle1 + 0.01) {
+			line12 = { 110,0 }, line21 = { 110,0 };
+			line12 = rotate_point(0, 0, angle1, line12);
+			line21 = rotate_point(0, 0, angle1 , line21);
+			oldangle1 = angle1;
+			if (angle2 == 0) {
+				line22.x = line12.x + line21.x;
+				line22.y = line12.y + line21.y;
+				
 			}
 		}
+		if (angle2 < oldangle2 - 0.1 || angle2 > oldangle2 + 0.1) {
+			line22.x = line12.x + line21.x;
+			line22.y = line12.y + line21.y;
+			line22 = rotate_point(line21.x,line21.y, (angle2), line22);
+			oldangle2 = angle2;
+			 
+		}
+		
+		printf("%f ", angle2);
+		dsrect1.x = line11.x;
+		dsrect1.y = line11.y-25;
+		dsrect2.x = line21.x;
+		dsrect2.y = line21.y-25;
+		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 0);
+		SDL_RenderCopyEx(renderer, roboArm1T, NULL, &dsrect1, angle1, &line11, SDL_FLIP_NONE);
+		if(angle2==0)
+			SDL_RenderCopyEx(renderer, roboArm1T, NULL, &dsrect2, angle1, &line21, SDL_FLIP_NONE);
+		else SDL_RenderCopyEx(renderer, roboArm1T, NULL, &dsrect2, angle2, &line21, SDL_FLIP_NONE);
+		SDL_RenderDrawLine(renderer, line11.x, line11.y, line12.x, line12.y);
+		SDL_RenderDrawLine(renderer, line21.x, line21.y, line22.x, line22.y);
+		SDL_RenderDrawRect(renderer, &dsrect2);
+		solveIK(&angle1, &angle2, false, 100, 100, mousex, mousey);
+		angle1 = get_degrees(angle1);
+		angle2 = get_degrees(angle2);
+		SDL_RenderPresent(renderer);
+		SDL_Delay(16);
+
 	}
+		
 
 	
     return 0;
